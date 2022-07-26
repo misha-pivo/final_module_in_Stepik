e230d5b12f2d7781a36298fcea4144b3ce7a53dc
misha-pivo <109038508+misha-pivo@users.noreply.github.com>
2022-07-27 00:31:51 +0500

    Optimisation and upgrade code for test main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)
