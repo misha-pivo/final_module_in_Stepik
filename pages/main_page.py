049099f988d79ccd3f0b3ee15ca686357046f39c
misha-pivo <109038508+misha-pivo@users.noreply.github.com>
2022-07-29 23:41:16 +0500

    4_2_10
    update files after complete example in lesson stepik 4_2_10

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
         
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"      
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"      
         
