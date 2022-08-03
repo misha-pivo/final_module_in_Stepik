c148b2faab7e8d5f98a1e4778e009b5f5b703ea7
misha-pivo <109038508+misha-pivo@users.noreply.github.com>
2022-08-03 22:20:29 +0500

    update tests after step 4_4_4

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,53 @@
-from selenium import webdriver
+import math
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver import Remote
 from selenium.common.exceptions import NoSuchElementException
+import time
 
-class BasePage():
-    def __init__(self, browser, url, timeout=10):
+
+class BasePage:
+    def __init__(self, browser: Remote, url: str) -> None:
+        """Base class for the web page
+
+        :param selenium.webdriver.Remote browser: Selenium WebDriver instance
+        :param str url: Link to the page
+        """
+        super().__init__()
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(10)
 
-    def open(self):
+    def open(self) -> None:
+        """Open the given web page in the browser
+
+        :return: None
+        """
         self.browser.get(self.url)
 
-def is_element_present(self, how, what):
-    try:
-        self.browser.find_element(how, what)
-    except (NoSuchElementException):
-        return False
-    return True
+    def is_element_present(self, how: str, what: str) -> bool:
+        """
+
+        :param how:
+        :param what:
+        :return:
+        """
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+
+        return True
+
+    def solve_quiz_and_get_code(self) -> None:
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        time.sleep(5)
+        try:
+            alert = self.browser.switch_to.alert
+            print(f"Your code: {alert.text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
